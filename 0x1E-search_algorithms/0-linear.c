c7c226bb743bd770023071d9f221b1a3a8ff8f68
Yohannes Tadiwos <yohannes812gk@gmail.com>
2022-10-05 05:49:10 -0700

    completed 1-binary.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,6 +6,8 @@
  * @size: size_t
  * @value: value
  * Return: int, index of the value
+ *
+ * Description: prints each array being searched
  */
 
 int linear_search(int *array, size_t size, int value)
@@ -20,6 +22,21 @@ for (i = 0; i < size; i++)
 	printf("Value checked array[%li] = [%i]\n", i, array[i]);
 	if (array[i] == value)
 		return (i);
-}
-return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", aeeat[i]);
+		print("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+	return (-1);
 }
